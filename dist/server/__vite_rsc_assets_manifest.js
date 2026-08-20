1965016c1d54a974cc4f3c16eff2d3ebc95bb9be
Naweedhag <navidhaghighi2013@gmail.com>
2026-08-20 09:48:47 +0330

    feature/shadcn added

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,58 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-DlqPGCaV.js\")",
+  "clientReferenceDeps": {
+    "6efdf509a785": {
+      "js": [
+        "/assets/page-ViUtDt3v.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/tooltip-BCKSxolx.js",
+        "/assets/framework-DjPHiq1u.js",
+        "/assets/index-DlqPGCaV.js"
+      ],
+      "css": []
+    },
+    "b6a451a68c84": {
+      "js": [
+        "/assets/providers-wDXY9hHo.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/tooltip-BCKSxolx.js",
+        "/assets/framework-DjPHiq1u.js",
+        "/assets/index-DlqPGCaV.js"
+      ],
+      "css": []
+    },
+    "593f344dc510": {
+      "js": [
+        "/assets/index-DlqPGCaV.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-DjPHiq1u.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/assets/layout-segment-context-DxbBbqUZ.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-DlqPGCaV.js",
+        "/assets/framework-DjPHiq1u.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/assets/index-DlqPGCaV.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-DjPHiq1u.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-D11zTtBa.css"
+      ]
+    }
+  }
+}
